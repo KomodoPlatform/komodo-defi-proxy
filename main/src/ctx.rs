85f4df45f95f77af67cd37f424889d87619e2916
ozkanonur <work@onurozkan.dev>
2022-11-18 14:04:24 +0300

    update config structure
    Signed-off-by: ozkanonur <work@onurozkan.dev>

--- main/src/ctx.rs
+++ main/src/ctx.rs
@@ -21,13 +21,13 @@ pub(crate) struct AppConfig {
     pub(crate) token_expiration_time: Option<i64>,
     pub(crate) proxy_routes: Vec<ProxyRoute>,
     pub(crate) rate_limiter: RateLimiter,
-    pub(crate) nodes: Vec<Node>,
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub(crate) struct ProxyRoute {
     pub(crate) inbound_route: String,
     pub(crate) outbound_route: String,
+    pub(crate) authorized: bool,
     pub(crate) allowed_methods: Vec<String>,
 }
 
@@ -55,12 +55,6 @@ impl AppConfig {
         Ok(serde_json::from_str(&file)?)
     }
 
-    pub(crate) fn get_node(&self, ticker: String) -> Option<&Node> {
-        (&self.nodes)
-            .iter()
-            .find(|node| node.coins.contains(&ticker))
-    }
-
     pub(crate) fn token_expiration_time(&self) -> i64 {
         self.token_expiration_time
             .unwrap_or(DEFAULT_TOKEN_EXPIRATION_TIME)
@@ -79,11 +73,13 @@ pub(crate) fn get_app_config_test_instance() -> AppConfig {
             ProxyRoute {
                 inbound_route: String::from("/test"),
                 outbound_route: String::from("https://komodoplatform.com"),
+                authorized: false,
                 allowed_methods: Vec::default(),
             },
             ProxyRoute {
                 inbound_route: String::from("/test-2"),
                 outbound_route: String::from("https://atomicdex.io"),
+                authorized: false,
                 allowed_methods: Vec::default(),
             },
         ]),
@@ -94,18 +90,6 @@ pub(crate) fn get_app_config_test_instance() -> AppConfig {
             rp_30_min: 555,
             rp_60_min: 555,
         },
-        nodes: Vec::from([
-            ctx::Node {
-                coins: vec![String::from("ETH")],
-                url: String::from("https://dummy-address"),
-                authorized: false,
-            },
-            ctx::Node {
-                coins: vec![String::from("KMD")],
-                url: String::from("https://dummy-address2"),
-                authorized: false,
-            },
-        ]),
     }
 }
 
@@ -121,11 +105,13 @@ fn test_app_config_serialzation_and_deserialization() {
             {
                 "inbound_route": "/test",
                 "outbound_route": "https://komodoplatform.com",
+                "authorized": false,
                 "allowed_methods": []
             },
             {
                 "inbound_route": "/test-2",
                 "outbound_route": "https://atomicdex.io",
+                "authorized": false,
                 "allowed_methods": []
             }
         ],
@@ -135,19 +121,7 @@ fn test_app_config_serialzation_and_deserialization() {
             "rp_15_min": 555,
             "rp_30_min": 555,
             "rp_60_min": 555
-        },
-        "nodes": [
-            {
-                "coins": ["ETH"],
-                "url": "https://dummy-address",
-                "authorized": false,
-            },
-            {
-                "coins": ["KMD"],
-                "url": "https://dummy-address2",
-                "authorized": false,
-            }
-        ]
+        }
     });
 
     let actual_config: AppConfig = serde_json::from_str(&json_config.to_string()).unwrap();
@@ -168,17 +142,4 @@ fn test_from_fs() {
     let actual = AppConfig::from_fs().unwrap();
     let expected = get_app_config_test_instance();
     assert_eq!(actual, expected);
-}
-
-#[test]
-fn test_get_node() {
-    let cfg = get_app_config_test_instance();
-
-    let node = cfg.get_node(String::from("ETH")).unwrap();
-    assert!(node.coins.contains(&"ETH".into()));
-    assert_eq!(node.url, "https://dummy-address");
-
-    let node = cfg.get_node(String::from("KMD")).unwrap();
-    assert!(node.coins.contains(&"KMD".into()));
-    assert_eq!(node.url, "https://dummy-address2");
 }
--- main/src/security/proof_of_funding.rs
+++ main/src/security/proof_of_funding.rs
@@ -1,5 +1,5 @@
 use super::*;
-use ctx::AppConfig;
+use ctx::{AppConfig, ProxyRoute};
 use http::RpcPayload;
 use rpc::Json;
 use serde_json::json;
@@ -8,7 +8,6 @@ use sign::SignOps;
 #[derive(Debug)]
 pub(crate) enum ProofOfFundingError {
     InvalidSignedMessage,
-    RpcClientNotFound,
     InsufficientBalance,
     ErrorFromRpcCall,
     RpcCallFailed(String),
@@ -17,29 +16,29 @@ pub(crate) enum ProofOfFundingError {
 pub(crate) async fn verify_message_and_balance(
     cfg: &AppConfig,
     payload: &RpcPayload,
+    proxy_route: &ProxyRoute,
 ) -> Result<(), ProofOfFundingError> {
     if let Ok(true) = payload.signed_message.verify_message() {
-        if let Some(node) = cfg.get_node(payload.signed_message.coin_ticker.clone()) {
-            let rpc_payload = json!({
-                "id": 1,
-                "jsonrpc": "2.0",
-                "method": "eth_getBalance",
-                "params": [payload.signed_message.address, "latest"]
-            });
+        let rpc_payload = json!({
+            "id": 1,
+            "jsonrpc": "2.0",
+            "method": "eth_getBalance",
+            "params": [payload.signed_message.address, "latest"]
+        });
 
-            let rpc_client = rpc::RpcClient::new(node.url.clone());
+        let rpc_client = rpc::RpcClient::new(proxy_route.outbound_route.clone());
 
-            match rpc_client.send(cfg, rpc_payload, node.authorized).await {
-                Ok(res) if res["result"] != Json::Null && res["result"] != "0x0" => return Ok(()),
-                Ok(res) if res["error"] != Json::Null => {
-                    return Err(ProofOfFundingError::ErrorFromRpcCall);
-                }
-                Ok(_) => return Err(ProofOfFundingError::InsufficientBalance),
-                Err(e) => return Err(ProofOfFundingError::RpcCallFailed(e.to_string())),
-            };
+        match rpc_client
+            .send(cfg, rpc_payload, proxy_route.authorized)
+            .await
+        {
+            Ok(res) if res["result"] != Json::Null && res["result"] != "0x0" => return Ok(()),
+            Ok(res) if res["error"] != Json::Null => {
+                return Err(ProofOfFundingError::ErrorFromRpcCall);
+            }
+            Ok(_) => return Err(ProofOfFundingError::InsufficientBalance),
+            Err(e) => return Err(ProofOfFundingError::RpcCallFailed(e.to_string())),
         };
-
-        return Err(ProofOfFundingError::RpcClientNotFound);
     }
 
     Err(ProofOfFundingError::InvalidSignedMessage)
