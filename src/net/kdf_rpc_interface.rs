d8a75e660be3782e306b8f5d2adc69726fcd5b92
onur-ozkan <work@onurozkan.dev>
2024-09-26 11:33:04 +0300

    sync the upstream changes
    Signed-off-by: onur-ozkan <work@onurozkan.dev>

--- src/net/kdf_rpc_interface.rs
+++ src/net/kdf_rpc_interface.rs
@@ -11,14 +11,14 @@ pub(crate) async fn version_rpc(cfg: &AppConfig) -> GenericResult<serde_json::Va
 
 pub(crate) async fn peer_connection_healthcheck_rpc(
     cfg: &AppConfig,
-    peer_id: &str,
+    peer_address: &str,
 ) -> GenericResult<serde_json::Value> {
     let payload = serde_json::json!({
         "userpass": cfg.kdf_rpc_password,
         "method": "peer_connection_healthcheck",
         "mmrpc": "2.0",
         "params": {
-            "peer_id": peer_id
+            "peer_address": peer_address
         }
     });
 
